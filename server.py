d1f6cdb407ae9b8e0405eaeeb19161683054ad58
mordechai342 <mordechaif.053@gmail.com>
2025-02-15 22:19:56 +0200

    Update server.py

--- server.py
+++ server.py
@@ -7,5 +7,7 @@ def home():
     return jsonify({"message": "Flask עובד!"})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    import os
+    port = int(os.environ.get("PORT", 5000))  # שימוש בפורט של Render
+    app.run(host='0.0.0.0', port=port)
 
